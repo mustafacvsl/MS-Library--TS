2e7409c4d02afdc2b9143b6434139280901dde4c
Mustafa <117647188+mustafacvsl@users.noreply.github.com>
2023-11-21 15:19:16 +0300

    update inversfiy

--- src/infrastructure/inversify.ts
+++ src/infrastructure/inversify.ts
@@ -7,6 +7,9 @@ import BookApplicationService from '../ApplicationService/BookApplicationService
 import AuthRepository from '../Domain/User/Auth.repository';
 import BookRepository from '../Domain/Book/Book.repository';
 import { AuthApplicationService } from '../ApplicationService/AuthApplicationService';
+import ExecutiveService from '../Domain/Executive/executive.service';
+import { ExecutiveApplicationService } from '../ApplicationService/ExecutiveApplicationService';
+import { ExecutiveController } from '../Controller/executive.controller';
 
 const container = new Container();
 container.bind<AuthApplicationService>(AuthApplicationService).toSelf();
@@ -17,5 +20,8 @@ container.bind<BookService>(BookService).toSelf();
 container.bind<BookApplicationService>(BookApplicationService).toSelf();
 container.bind<AuthRepository>(AuthRepository).toSelf();
 container.bind<BookRepository>(BookRepository).toSelf();
+container.bind<ExecutiveService>(ExecutiveService).toSelf();
+container.bind<ExecutiveApplicationService>(ExecutiveApplicationService).toSelf();
+container.bind<ExecutiveController>(ExecutiveController).toSelf();
 
 export default container;
